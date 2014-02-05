de2f059798b496af6c1bd3b1afe0ea6e7e5ef572
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-02-05 17:27:38 -0200

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+cute-problem-chooser
+====================
+
+Copyright 2014 - Abel Soares Siqueira - abel.s.siqueira@gmail.com
+Gpl v3.
+
+Based on the CUTEr Problem Chooser.
+Scripts to select specific CUTE(r|st) problems, using the description provided
+in the website.
+
+Contributors of the original project:
+Leandro Prudente - lfprudente@gmail.com
+Raniere Gaia Costa da Silva - r.gaia.cs@gmail.com
+Me - my mail
+
+Overview
+--------
+
+Installing and Running
+----------------------
+
+License
+-------
+
+This software is available under the GNU Public License v.3,
+which can be seen in COPYING.
+Unless otherwise noted, all source is under this license.
---
+++ getsiflist.rb
@@ -0,0 +1,83 @@
+#!/usr/bin/ruby -w
+
+def case_objfun (objfun)
+  case objfun
+  when "N"
+    "notdef"
+  when "C"
+    "cnstnt"
+  when "L"
+    "linear"
+  when "Q"
+    "quadrt"
+  when "S"
+    "sumsqr"
+  when "O"
+    "others"
+  else
+    raise objfun + " is not in the expected cases"
+  end
+end
+
+def case_constr (constr)
+  case constr
+  when "U"
+    "unc"
+  when "X"
+    "fix"
+  when "B"
+    "bnd"
+  when "N"
+    "net"
+  when "L"
+    "lin"
+  when "Q"
+    "qdt"
+  when "O"
+    "oth"
+  else
+    raise constr + " is not in the expected cases"
+  end
+end
+
+def case_regular (regular)
+  case 
+  when "R"
+    "reg"
+  when "I"
+    "irr"
+  else
+    raise regular + " is not is the expected cases"
+  end
+end
+
+require 'open-uri'
+require 'nokogiri'
+
+doc = Nokogiri::HTML(open('http://www.cuter.rl.ac.uk/Problems/mastsif.shtml'))
+td = doc.xpath("//td")
+
+td.each_slice(3) { |a| 
+  name = a[0].content.strip
+  classification = a[2].content.strip.split('-')
+
+  begin
+    objfun  = case_objfun(classification[0][0])
+    constr  = case_constr(classification[0][1])
+    regular = case_regular(classification[0][2])
+  rescue Exception => e
+    puts "In line " + a[0].content.strip + " " + a[2].content.strip
+    raise e.message
+  end
+  deriv   = classification[0][3]
+
+  origin   = classification[1][0]
+  internal = classification[1][1]
+
+  nvar = classification[2]
+  ncon = classification[3]
+
+
+  printf("%-9s %9s %9s %6s %3s %3s %s %s %s\n", name, nvar, ncon, objfun, constr, regular,
+    deriv, origin, internal)
+}
